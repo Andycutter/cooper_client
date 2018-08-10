4b963523b17ae89df939a76d64351926d8fdd560
AndreasMN <andycutter@godmail.dk>
2018-08-10 13:33:03 +0200

    setup angular2-token

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
+import { Angular2TokenService } from 'angular2-token';
 
 @Component({
   templateUrl: 'app.html'
@@ -15,7 +16,16 @@ export class MyApp {
 
   pages: Array<{title: string, component: any}>;
 
-  constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
+  constructor(
+    public platform: Platform,
+    public statusBar: StatusBar,
+    public splashScreen: SplashScreen,
+    private _tokenService: Angular2TokenService
+  ) {
+    this._tokenService.init({
+      apiBase: 'https://amn-cooper-api.herokuapp.com/api/v1'
+    });
+    
     this.initializeApp();
 
     // used for an example of ngFor and navigation
